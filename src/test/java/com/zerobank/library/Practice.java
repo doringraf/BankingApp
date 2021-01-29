c589c27a0c2d9910fcbab53ee94d4025df96bb4d
doringraf <35940476+doringraf@users.noreply.github.com>
2021-01-28 21:58:08 -0500

    Update Practice.java

--- src/test/java/com/zerobank/library/Practice.java
+++ src/test/java/com/zerobank/library/Practice.java
@@ -7,21 +7,21 @@ import java.util.List;
 public class Practice {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hellssssso world");
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("ssssshello world");
         System.out.println("hello world");
+        System.out.println("hello worsssld");
         System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
-        System.out.println("hello world");
+        System.out.println("hellssssso Cybertek");
         System.out.println("hello Cybertek");
         System.out.println("hello Cybertek");
         System.out.println("hello Cybertek");
         System.out.println("hello Cybertek");
         System.out.println("hello Cybertek");
-        System.out.println("hello Cybertek");
-        System.out.println("hello Cybertek");
+        System.out.println("hellossssss Cybertek");
     }
 }
